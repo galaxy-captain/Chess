536f5ce3baa86a5c822bfccc44e9e3abd2b4fd4b
银河战舰的数据仓库 <goskkyer@gmail.com>
2016-08-30 16:47:03 +0800

    添加士的规则

--- src/chinesechess/rule/ShiRule.java
+++ src/chinesechess/rule/ShiRule.java
@@ -1,17 +1,54 @@
 package chinesechess.rule;
 
+import chinesechess.board.Groups;
 import chinesechess.rule.abs.ChineseChessRule;
 import org.ddd.board.AbsBoard;
 import org.ddd.board.BoardPoint;
 import org.ddd.piece.impl.IPiece;
 
 /**
+ * 士(SHI)的移动规则
+ * <p>
  * Created by ddd on 16/8/27.
  */
 public class ShiRule extends ChineseChessRule {
 
     @Override
     protected boolean moveRule() {
-        return false;
+
+        int toPointX = getPoint().getX();
+        int pointX = getPiece().getPoint().getX();
+
+        int toPointY = getPoint().getY();
+        int pointY = getPiece().getPoint().getY();
+
+        // 只能斜着移动一个格子
+        if (!(Math.abs(toPointX - pointX) == 1 && Math.abs(toPointY - pointY) == 1)) return false;
+
+        // 只能在范围内移动
+        if (!moveRange(toPointX, toPointY)) return false;
+
+        if (getBoard().getPiece(getPoint()) != null &&
+                getPiece().getGroup() == getBoard().getPiece(getPoint()).getGroup())
+            return false;
+
+        return true;
     }
+
+    /**
+     * 士的移动范围
+     *
+     * @param toX
+     * @param toY
+     * @return
+     */
+    private boolean moveRange(int toX, int toY) {
+
+        if (getPiece().getGroup() == Groups.GROUP_BLUE && toX <= 2 && toY >= 3 && toY <= 5) return true;
+        else if (getPiece().getGroup() == Groups.GROUP_RED && toX >= 7 && toY >= 3 && toY <= 5) return true;
+
+        return false;
+
+    }
+
 }
